b2395423e44b2bc5c432d8b2809bbeabc63d1731
lopera.william@gmail.com <William>
2022-03-15 13:26:37 -0500

    ajustes por unpkg - otros

--- single-app/src/mc-root-config.js
+++ single-app/src/mc-root-config.js
@@ -1,53 +1,24 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () => System.import("@single-spa/welcome"),
-  activeWhen: (location) => location.pathname === "/",
-});
-
-registerApplication({
-  name: "@mc/react-single",
-  app: () => System.import("@mc/react-single"),
-  activeWhen: (location) => location.pathname === "/react-single",
-});
-
-registerApplication({
-  name: "@mc/react-multiples",
-  app: () => System.import("@mc/react-multiples"),
-  activeWhen: ["/react-multiples"],
-});
-
-registerApplication({
-  name: "@mc/react-lazy",
-  app: () => System.import("@mc/react-lazy"),
-  activeWhen: ["/react-lazy"],
-});
-
-registerApplication({
-  name: "@mc/react-parcel",
-  app: () => System.import("@mc/react-parcel"),
-  activeWhen: (location) => location.pathname === "/react-parcel",
-});
-
-registerApplication({
-  name: "@mc/react-route",
-  app: () => System.import("@mc/react-route"),
-  activeWhen: (location) => location.pathname === "/react-route",
-});
-
-registerApplication({
-  name: "@mc/react-header",
-  app: () => System.import("@mc/react-header"),
-  activeWhen: ["/"],
-});
-
-// registerApplication({
-//   name: "@mc/navbar",
-//   app: () => System.import("@mc/navbar"),
-//   activeWhen: ["/"]
-// });
-
-start({
-  urlRerouteOnly: true,
-});
+fetch("https://run.mocky.io/v3/65b7ebe1-a792-43fa-8b35-cca01ea498f5")
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+    data.applications.forEach((app) => {
+      registerApplication({
+        name: app.name,
+        app: () => System.import(app.package),
+        activeWhen: app.exact
+          ? (location) => location.pathname === app.activeWhen
+          : [app.activeWhen],
+      });
+    });
+  })
+  .catch((error) => {
+    console.log("##=> Error:", error);
+  })
+  .finally(() => {
+    start({
+      urlRerouteOnly: true,
+    });
+  });
